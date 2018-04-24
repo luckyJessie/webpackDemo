83d9be1acf3018f6844e8d11899c4737e4f33f37
luckyJessie <https://github.com/dynicyan/mpaiment.git>
2018-04-24 18:36:01 +0800

    edit

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+/**
+ * Created by caofan on 2018/4/24.
+ */
+module.exports = {
+  plugins: [
+    require('autoprefixer')
+  ]
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 /**
  * Created by caofan on 2018/4/24.
  */
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry:  __dirname + "/app/main.js",//已多次提及的唯一入口文件
   output: {
-    path: __dirname + "/public",//打包后的文件存放的地方
+    path: __dirname + "/build",//打包后的文件存放的地方
     filename: "bundle.js"//打包后输出文件的文件名
   },
   devtool: 'eval-source-map',
@@ -12,6 +14,7 @@ module.exports = {
     contentBase: "./public",//本地服务器所加载的页面所在的目录
     historyApiFallback: true,//不跳转
     inline: true,//实时刷新
+    hot: true
   },
   module: {
     rules: [
@@ -30,14 +33,20 @@ module.exports = {
           }, {
             loader: "css-loader",
             options: {
-              modules: true, // 指定启用css modules
-              localIdentName: '[name]__[local]--[hash:base64:5]' // 指定css的类名格式
+              modules: true
             }
+          }, {
+            loader: "postcss-loader"
           }
         ]
       }
     ]
   },
   plugins: [
+    new webpack.BannerPlugin('版权所有，翻版必究'),
+    new HtmlWebpackPlugin({
+      template: __dirname + "/app/index.tmpl.html"//new 一个这个插件的实例，并传入相关的参数
+    }),
+    new webpack.HotModuleReplacementPlugin()//热加载插件
   ]
 }
